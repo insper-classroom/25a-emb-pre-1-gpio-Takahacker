e41660cbbb2e127d0d1919c67dc698a5b01e9635
Gustavo Takahashi <gustavoet@al.insper.edu.br>
2025-02-11 19:53:52 -0300

    ex5

--- exe5/main.c
+++ exe5/main.c
@@ -4,6 +4,7 @@
 
 const int BTN_PIN = 26;
 const int BTN_PIN_2 = 7;
+const int DEBOUNCE_DELAY = 10; // ms
 
 int main() {
     stdio_init_all();
@@ -15,17 +16,34 @@ int main() {
     gpio_init(BTN_PIN_2);
     gpio_set_dir(BTN_PIN_2, GPIO_IN);
     gpio_pull_up(BTN_PIN_2);
+
     int cnt_1 = 0;
     int cnt_2 = 0;
 
+    bool btn1_pressed = false;
+    bool btn2_pressed = false;
+
     while (true) {
-
+        
         if (!gpio_get(BTN_PIN)) {
-            printf("Botao 1: %d\n", cnt_1++);
+            if (!btn1_pressed) {
+                btn1_pressed = true;
+                printf("Botao 1: %d\n", cnt_1++);
+            }
+        } else {
+            btn1_pressed = false;
         }
 
+        
         if (!gpio_get(BTN_PIN_2)) {
-            printf("Botao 2: %d\n", cnt_2++);
+            if (!btn2_pressed) {
+                btn2_pressed = true;
+                printf("Botao 2: %d\n", cnt_2++);
+            }
+        } else {
+            btn2_pressed = false;
         }
+
+        sleep_ms(DEBOUNCE_DELAY);
     }
 }
