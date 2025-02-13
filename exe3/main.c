44f6eeab9aca252a52490235ceb4f04aefb72078
Gustavo Takahashi <gustavoet@al.insper.edu.br>
2025-02-13 15:42:03 -0300

    3

--- exe3/main.c
+++ exe3/main.c
@@ -7,10 +7,8 @@ const int LED_PIN_G = 6;
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
 
-int main()
-{
+int main(){
     stdio_init_all();
-
     gpio_init(LED_PIN_R);
     gpio_init(LED_PIN_G);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
@@ -42,5 +40,6 @@ int main()
             i_G = !i_G;
             gpio_put(LED_PIN_G, i_G);
             sleep_ms(200); 
+        }
     }
 }
